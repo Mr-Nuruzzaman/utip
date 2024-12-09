4f25c164e7b41ef4447069a947337c1b1291bca3
Mr-Nuruzzaman <nuruzzaman.prog@gmail.com>
2024-12-09 10:06:33 +0600

    Initialize UTip App

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTip App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const  UTip(),
+    );
+  }
+}
+
+class UTip extends StatefulWidget {
+  const UTip({super.key});
+
+  @override
+  State<UTip> createState() => _UTipState();
+}
+
+class _UTipState extends State<UTip> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(
+        title: const Text('UTip'),
+      ),
+      body: Column(
+         children: [
+          Container(
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 163, 118, 240),
+              borderRadius: BorderRadius.circular(10), 
+            ),
+            child: const Column(
+              children: [
+                Text("Total per person"),
+                Text("\$23.35 "),
+              ],
+            )
+            )
+        ],
+      )
+    );
+  }
+}
+
+
+
+
+ 
